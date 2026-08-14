90ac22be34e287b4a5550ddd883204931177c658
shershenARP <187065744+shershenARP@users.noreply.github.com>
2026-08-14 16:35:13 +0300

    Update ЫolarToolsMod.scala

--- build.ps1
+++ build.ps1
@@ -1,6 +1,1 @@
-gradle clean build --no-daemon
-if ($LASTEXITCODE -eq 0) {
-    Write-Host "`n✅ Мод собран: build\libs\GrifersTools-1.0.jar" -ForegroundColor Green
-} else {
-    Write-Host "`n❌ Сборка не удалась" -ForegroundColor Red
-}
+
